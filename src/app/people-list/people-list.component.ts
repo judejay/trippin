02d7a5a8afb98c37d4211cffc58191fdd2412979
judejay <judejay26@gmail.com>
2024-03-01 14:05:41 +0000

    feat: async pipe on observable

--- src/app/people-list/people-list.component.ts
+++ src/app/people-list/people-list.component.ts
@@ -1,12 +1,16 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import { tap } from 'rxjs';
 
 @Component({
   selector: 'app-people-list',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './people-list.component.html',
-  styleUrl: './people-list.component.scss'
+  styleUrl: './people-list.component.scss',
 })
 export class PeopleListComponent {
-
+  public nameFilter = new FormControl('');
+  public input$ = this.nameFilter.valueChanges.pipe(tap((f) => console.log(f)));
 }
